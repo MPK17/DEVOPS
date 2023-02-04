ff6ea58647d55aabd43ca8e6ad45d1d1e76a5f33
MPK17 <61377509+MPK17@users.noreply.github.com>
2023-02-04 02:04:32 +0100

    comit devops

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+
+
+
+
+
+import '../assets/js/chart-area-demo';
+import '../assets/js/chart-bar-demo';
+import './js/chart-pie-demo';
+import './js/datatables-demo';
+import './js/datatables-simple-demo';
+import './js/scripts';
+import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js';
+import'https://cdn.jsdelivr.net/npm/simple-datatables@latest';
+
+
+
+
+// start the Stimulus application
